a5e6a710eef20426b6e3890e4009efe4b3db9053
Ahsan Rehmani <rehmaniahsan15@gmail.com>
2023-06-05 16:35:21 +0500

    Installed theme for authentication

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,31 +1,105 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-    </div>
+@extends('layouts.guest')
+@section('title', 'Login')
 
-    @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-        </div>
-    @endif
-
-    <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('verification.send') }}">
-            @csrf
-
-            <div>
-                <x-primary-button>
-                    {{ __('Resend Verification Email') }}
-                </x-primary-button>
+@section('page-content')
+    <div class="nk-content ">
+        <div class="container nk-block nk-block-middle">
+            <div class="brand-logo pb-5">
+                <a href="html/index.html" class="logo-link">
+                    <img class="logo-light logo-img logo-img-lg" src="./images/logo.png" srcset="./images/logo2x.png 2x"
+                        alt="logo">
+                    <img class="logo-dark logo-img logo-img-lg" src="./images/logo-dark.png"
+                        srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
+                </a>
             </div>
-        </form>
+            <div class="nk-block-head">
+                <div class="nk-block-head-content">
+                    <h5 class="nk-block-title">
+                        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+                    </h5>
 
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
+                    @if (session('status') == 'verification-link-sent')
+                        <h5 class="nk-block-title">
+                            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                        </h5>
+                    @endif
+                    <div class="nk-block-des text-success">
+                        <p>You can now sign in with your new password</p>
+                    </div>
 
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                {{ __('Log Out') }}
-            </button>
-        </form>
+                    <div class="nk-auth-body">
+                        <form method="POST" action="{{ route('verification.send') }}">
+                            @csrf
+                            <div class="form-group">
+                                <button type="submit"
+                                    class="btn btn-lg btn-primary btn-block">{{ __('Resend Verification Email') }}</button>
+                            </div>
+                        </form>
+
+                        <form method="POST" action="{{ route('logout') }}">
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-lg btn-link btn-block">{{ __('Logout') }}</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="nk-footer nk-auth-footer-full">
+            <div class="container wide-lg">
+                <div class="row g-3">
+                    <div class="col-lg-6 order-lg-last">
+                        <ul class="nav nav-sm justify-content-center justify-content-lg-end">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Terms & Condition</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Privacy Policy</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Help</a>
+                            </li>
+                            <li class="nav-item dropup">
+                                <a class="dropdown-toggle dropdown-indicator has-indicator nav-link" data-toggle="dropdown"
+                                    data-offset="0,10"><span>English</span></a>
+                                <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">
+                                    <ul class="language-list">
+                                        <li>
+                                            <a href="#" class="language-item">
+                                                <img src="./images/flags/english.png" alt="" class="language-flag">
+                                                <span class="language-name">English</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="language-item">
+                                                <img src="./images/flags/spanish.png" alt="" class="language-flag">
+                                                <span class="language-name">Español</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="language-item">
+                                                <img src="./images/flags/french.png" alt="" class="language-flag">
+                                                <span class="language-name">Français</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="language-item">
+                                                <img src="./images/flags/turkey.png" alt="" class="language-flag">
+                                                <span class="language-name">Türkçe</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="nk-block-content text-center text-lg-left">
+                            <p class="text-soft">&copy; 2022 DashLite. All Rights Reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</x-guest-layout>
+@endsection
