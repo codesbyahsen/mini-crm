6e43ca780357aa6c3cdd8d6f56058a95c588bd59
Ahsan Rehmani <rehmaniahsan15@gmail.com>
2023-06-06 00:22:46 +0500

    Added admin seeders to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PermissionSeeder::class,
             RoleSeeder::class,
+            AdminSeeder::class,
+            SubAdminSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
 
