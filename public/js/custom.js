64855b2a005282049f95d44b2a5cf5d6b6d2564a
AHSEN ALEE <ahsenalee4@gmail.com>
2023-06-14 18:51:16 +0500

    Functional the text editor and Added view
    Added summernote text editor to the detail attribute of project and also added modal view to show project details.

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,8 +1,4 @@
-$(document).ready(function() {
-    $('.summernote').summernote({
-        height: 120
-    });
-  });
+// ============================== |> Global <| ============================== //
 
 /**
  | ----------------------------------------------------------------
@@ -16,6 +12,30 @@ $(document).ready(function () {
         $("#preloader").hide();
     }, 2000);
 });
+
+/**
+ | ----------------------------------------------------------------
+ |  Initialize summernote
+ | ----------------------------------------------------------------
+ */
+$(document).ready(function () {
+    $('.summernote').summernote({
+        height: 120
+    });
+});
+
+/**
+ | ----------------------------------------------------------------
+ |  Change the format of date
+ | ----------------------------------------------------------------
+ |
+ | It changes the date format and return as ISO 8601
+ |
+ */
+const changeDateFormat = (date) => {
+    var date = new Date(date);
+    return date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
+}
 
 /**
  | ----------------------------------------------------------------
@@ -56,6 +76,8 @@ $(document).ready(function () {
         $("body").removeClass("dark-mode");
     }
 });
+
+// ============================== |> User Profile Module <| ============================== //
 
 /**
  | ----------------------------------------------------------------
@@ -311,7 +333,7 @@ $('#security-settings').submit(function (e) {
 });
 
 
-// ============================== |> Module One <| ============================== //
+// ============================== |> Company Module <| ============================== //
 
 /**
  | ----------------------------------------------------------------
@@ -629,7 +651,7 @@ $('#init-company-datatable').on('click', '.delete-button', function () {
     })
 });
 
-// ============================== |> Module Two <| ============================== //
+// ============================== |> Employee Module <| ============================== //
 
 /**
  | ----------------------------------------------------------------
@@ -950,7 +972,7 @@ $('#init-employee-datatable').on('click', '.delete-button', function () {
     })
 });
 
-// ============================== |> Module Three <| ============================== //
+// ============================== |> Project Module <| ============================== //
 
 /**
  | ----------------------------------------------------------------
@@ -1085,6 +1107,44 @@ const resetProjectFields = () => {
 
 /**
  | ----------------------------------------------------------------
+ |  Show project attributes
+ | ----------------------------------------------------------------
+ |
+ | It accepts the object parameter and shows the attributes
+ | of project
+ |
+ */
+const showProjectAttributes = (response) => {
+    $('.show-name').html(response?.data?.name);
+    $('.show-client-name').html(response?.data?.client_name);
+    $('.show-total-cost').html(response?.data?.total_cost);
+    $('.show-deadline').html(response?.data?.deadline);
+    $('.show-detail').html(response?.data?.detail);
+    var employees = response?.data?.employees;
+    employees.map(function (employee, index) {
+        $('.show-employees').append(employee?.first_name + ' ' + employee.last_name + ((employees.length - 1) == index ? '.' : ', '));
+    });
+}
+
+/**
+ | ----------------------------------------------------------------
+ |  Reset project attributes
+ | ----------------------------------------------------------------
+ |
+ | It resets the attributes of project
+ |
+ */
+const resetProjectAttributes = () => {
+    $('.show-name').html(null);
+    $('.show-client-name').html(null);
+    $('.show-total-cost').html(null);
+    $('.show-deadline').html(null);
+    $('.show-detail').html(null);
+    $('.show-employees').html(null);
+}
+
+/**
+ | ----------------------------------------------------------------
  |  Create project
  | ----------------------------------------------------------------
  |
@@ -1140,16 +1200,44 @@ $('.cancel-create-project-form').click(function () {
 
 /**
  | ----------------------------------------------------------------
- |  Change the format of date
+ |  Show project
  | ----------------------------------------------------------------
  |
- | It changes the date format and return as ISO 8601
+ | It shows the show project details and sends ajax request
+ | against the specific project
  |
  */
-const changeDateFormat = (date) => {
-    var date = new Date(date);
-    return date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
-}
+$('#init-project-datatable').on('click', '.view-details-button', function () {
+    $.ajax({
+        type: 'GET',
+        url: $(this).data('show-url'),
+        success: function (response) {
+            if (response.success === true) {
+                showProjectAttributes(response);
+            } else {
+                console.log(response);
+            }
+        },
+        error: function (xhr, status, error) {
+            console.log(xhr);
+            console.log(status);
+            console.log(error);
+        }
+    });
+});
+
+/**
+ | ----------------------------------------------------------------
+ |  Close the view of project
+ | ----------------------------------------------------------------
+ |
+ | It reset the html of the attributes when click on close
+ | modal
+ |
+ */
+$('.close-view-project').click(function () {
+    resetProjectAttributes();
+});
 
 /**
  | ----------------------------------------------------------------
